42dfdf7d4e7d86341b69bcc59be3334329a3d992
esmeurena <esme.urena27@gmail.com>
2025-05-14 13:12:55 -0700

    seeder fix

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,5 +1,12 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .transactions import seed_transactions, undo_transactions
+from .shoppingCarts import seed_shopping_carts, undo_shopping_carts
+from .reviews import seed_reviews, undo_reviews
+from .reviewImages import seed_review_images, undo_review_images
+from .products import seed_products, undo_products
+from .productImages import seed_product_images, undo_product_images
+from .favorites import seed_favorites, undo_favorites
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,9 +24,23 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_transactions()
+        undo_shopping_carts()
+        undo_reviews()
+        undo_review_images()
+        undo_products()
+        undo_product_images()
+        undo_favorites()
+        
     seed_users()
     # Add other seed functions here
-
+    seed_transactions()
+    seed_shopping_carts()
+    seed_reviews()
+    seed_review_images()
+    seed_products()
+    seed_product_images()
+    seed_favorites()
 
 # Creates the `flask seed undo` command
 @seed_commands.command("undo")
