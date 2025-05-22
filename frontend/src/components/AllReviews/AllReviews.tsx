77943c32a1f3a47a6bb253a59650ba87229cc108
esmeurena <esme.urena27@gmail.com>
2025-05-22 11:55:31 -0700

    get all reviews fix

--- frontend/src/components/AllReviews/AllReviews.tsx
+++ frontend/src/components/AllReviews/AllReviews.tsx
@@ -1,29 +1,30 @@
 import './AllReviews.css';
 import ReviewCard from '../ReviewCard';
-import { IAllReviews } from '../../redux/types/reviews';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 import OpenModalButton from '../OpenModalButton';
 import DeleteReviewModal from '../DeleteReviewModal';
 
-const AllReviews = ({reviews}: IAllReviews) => {
-    return (
-        <div id="all-reviews">
-            {reviews.length && reviews.map((review, i) => {
-                return (
-                    <div key={`${review.id}-${i}`} className='all-reviews-review'>
-                        <ReviewCard review={review} productId={0}/>
-
-                            <OpenModalButton
+const AllReviews = () => {
+const reviews = useSelector((state: RootState) => state.reviews.allReviews);
+  return (
+    <div id="all-reviews">
+      {reviews.length &&
+        reviews.map((review, i) => {
+          return (
+            <div key={`${review.id}-${i}`} className="all-reviews-review">
+              <ReviewCard review={review} productId={review.product_id} />
+              <OpenModalButton
                                 buttonText="Delete Review"
                                 buttonClassName="delete-btn"
                                 modalComponent={<DeleteReviewModal reviewId={review.id} />}
-                            />       
-                                             
-                        <hr id='all-reviews-line'></hr>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+                            /> 
+              <hr id="all-reviews-line"></hr>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default AllReviews;
