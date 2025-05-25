c024cf04a2739ca712b61f912e34ae37fe12d79f
SeymaErcan <seyma.ercan@gmail.com>
2025-05-25 14:09:36 -0400

    undo layout.tsx

--- frontend/src/router/Layout.tsx
+++ frontend/src/router/Layout.tsx
@@ -8,7 +8,7 @@ import FooterLinks from "../components/FooterLinks";
 
 export default function Layout():JSX.Element {
   const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(true);
   useEffect(() => {
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
