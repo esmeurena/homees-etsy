f02a02a1c2b98cdfcb06d34c75a37c3f25bbbf43
Oscar Page <ihighhigh@Oscars-MacBook-Pro.local>
2025-05-26 21:49:14 -0700

    CSS for Favorites

--- frontend/src/components/FavoritesPage/FavoritesPage.tsx
+++ frontend/src/components/FavoritesPage/FavoritesPage.tsx
@@ -35,11 +35,14 @@ const FavoritesPage: React.FC = () => {
                 {favorites.map((fav: IFavorite) => (
                     <li key={fav.id} className="favorite-item">
                         <div className="favorite-product-info" onClick={() => handleClickProduct(fav.product_id)}>
-                            <h2> Product #{fav.product_id}</h2>
-                            <p> Name: {fav.product?.name}</p>
+                            <img className="fav-image" src= {fav.product.product_images[0].url} />
+                            <div className='info-right'>
                             <p> Description: {fav.product?.description}</p>
+                            <p> Name: {fav.product?.name}</p>
                             <p> Rating: {fav.product?.avg_rating}</p>
-                            {/* {fav.product?.name && <p>{fav.product.name}</p>} */}
+                            <p> Price: ${fav.product?.price}</p>
+                            <p> {fav.product?.avg_rating} </p>
+                            </div>
                         </div>
                     </li>
                 ))}
